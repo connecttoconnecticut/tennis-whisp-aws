a36a3159bb17b48ddd36563f33d0ca6a22a6f9d3
connecttoconnecticut <boban96minic@gmail.com>
2019-04-18 12:26:37 +0200

    callback

--- src/websockets/connectionHandler.js
+++ src/websockets/connectionHandler.js
@@ -5,9 +5,9 @@ const config = require('../../config.json');
 //require('aws-sdk/clients/apigatewaymanagmentapi');
 
 const succesfullResponse = {
-    "statusCode": 200,
-    "body": 'ConnHandler -> Everything is alright...',
-    "isBase64Encoded" : false
+    statusCode: 200,
+    body: JSON.stringify({ status: 'OK'}),
+    isBase64Encoded : false
 }
 
 module.exports.connectionHandler = (event,context,callback) => {
@@ -17,27 +17,32 @@ module.exports.connectionHandler = (event,context,callback) => {
             if(succ){
                 console.log('ConnHandler -> Addconnection: Success!');
                 callback(null,(succesfullResponse));
-                context.succeed('Connect -> Done!');
             }else{
-                xcallback(null, JSON.stringify({
-                    "statusCode": 500,
-                    "body": 'Failed to connect to db: ' + JSON.stringify(connErr)
-                }));
-                context.succeed('Connect -> Done!');
-            };
-        });
-    }else if(event.requestContext.eventType === 'DISCONNECT'){
-        deleteConnection(event.requestContext.connectionId,function(connErr,succ){
-            if(succ){
-                console.log('ConnHandler -> Deleteconnection: Success!');
-                callback(null,JSON.stringify(succesfullResponse));
-                context.succeed('Disconnect -> Done!');
-            }else{
+                //context.succeed('Connection Failed.');
                 callback(null, JSON.stringify({
                     "statusCode": 500,
                     "body": 'Failed to connect to db: ' + JSON.stringify(connErr)
                 }));
-                context.succeed('Disconnect -> Error!');
+            };
+        });
+        
+        
+        //console.log('ConnHandler -> Addconnection: Success!');
+        //callback(null,(succesfullResponse));
+        //context.succeed('Success!');
+
+    }else if(event.requestContext.eventType === 'DISCONNECT'){
+        deleteConnection(event.requestContext.connectionId,function(connErr,succ){
+            if(succ){
+                console.log('ConnHandler -> Deleteconnection: Success!');
+                //context.succeed('Disconnect -> Done!');
+                callback(null,JSON.stringify(succesfullResponse));
+            }else{
+                //context.succeed('Disconnect -> Error!');
+                callback(null, JSON.stringify({
+                    "statusCode": 500,
+                    "body": 'Failed to connect to db: ' + JSON.stringify(connErr)
+                }));
             };
         });
     }
@@ -49,12 +54,20 @@ function addConnection (connection_id,callback){
     console.log('Add connection -> Called!');
     let conn = mysql.createConnection(config.DATABASE_CONNECTION);
     let insertquery = `INSERT INTO chat (connection_id) VALUES ('${connection_id}');`;
-    conn.query(insertquery,function(err){
+    conn.connect(function(err){
         if(err){
-            console.log('ConnHandler -> Addconnection err: ' + err);
+            console.log('ConnHandler -> Connect to DB err: ' + err);
             callback(err,false);
         }else{
-            callback(null,true);
+            conn.query(insertquery,function(err){
+                if(err){
+                    console.log('ConnHandler -> Addconnection err: ' + err);
+                    callback(err,false);
+                }else{
+                    console.log('Insert to DB -> Success!');
+                    callback(null,true);
+                }
+            })
         }
     })
 }
@@ -68,7 +81,7 @@ function deleteConnection (connection_id,callback){
             console.log('ConnHandler -> Deleteconnection err: ' + err);
             callback(err,false);
         }else{
-            //console.log('ConnHandler -> Deleteconnection: Success!');
+            console.log('Delete from DB -> Success!');
             callback(null,true);
         }
     })
