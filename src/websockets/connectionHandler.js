e78e67e0dd6a142cb5dd9a19c29522b31b73763a
connecttoconnecticut <boban96minic@gmail.com>
2019-04-24 16:19:01 +0200

    Update connectionHandler.js

--- src/websockets/connectionHandler.js
+++ src/websockets/connectionHandler.js
@@ -2,8 +2,6 @@
 const mysql = require('mysql');
 const config = require('../../config.json');
 
-//require('aws-sdk/clients/apigatewaymanagmentapi');
-
 const succesfullResponse = {
     statusCode: 200,
     body: JSON.stringify({ status: 'OK'}),
@@ -11,14 +9,11 @@ const succesfullResponse = {
 };
 
 module.exports.connectionHandler = (event,context,callback) => {
-    console.log('ConnHandler -> event:: ' + JSON.stringify(event));
     if(event.requestContext.eventType === 'CONNECT'){
         addConnection(event.requestContext.connectionId,function(connErr,succ){
             if(succ){
-                console.log('ConnHandler -> Addconnection: Success!');
                 callback(null,succesfullResponse);
             }else{
-                //context.succeed('Connection Failed.');
                 callback(null, JSON.stringify({
                     "statusCode": 500,
                     "body": 'Failed to connect to db: ' + JSON.stringify(connErr)
@@ -40,10 +35,7 @@ module.exports.connectionHandler = (event,context,callback) => {
     }
 }
 
-//let conn = mysql.createConnection(config.DATABASE_CONNECTION);
-
 function addConnection (connection_id,callback){
-    console.log('Add connection -> Called!');
     let insertquery = `INSERT INTO chat (connection_id) VALUES ('${connection_id}');`;
     let conn = mysql.createConnection(config.DATABASE_CONNECTION);
     conn.connect(function(err){
@@ -57,7 +49,6 @@ function addConnection (connection_id,callback){
                     conn.end();
                     callback(err,false);
                 }else{
-                    console.log('Insert to DB -> Success!');
                     conn.end();
                     callback(null,true);
                 }
@@ -67,12 +58,10 @@ function addConnection (connection_id,callback){
 }
 
 function deleteConnection (connection_id,callback){
-    console.log('Delete connection -> Called!');
     let deletequery = `DELETE FROM chat WHERE connection_id='${connection_id}';`;
     let conn = mysql.createConnection(config.DATABASE_CONNECTION);
     conn.connect(function(err){
         if(err){
-            console.log('ConnHandler -> Disconnect to DB err: ' + err);
             callback(err,false);
         }else{
             conn.query(deletequery,function(err){
@@ -81,7 +70,6 @@ function deleteConnection (connection_id,callback){
                     conn.end();
                     callback(err,false);
                 }else{
-                    console.log('Delete from DB -> Success!');
                     conn.end();
                     callback(null,true);
                 }
